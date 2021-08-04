ea098086dfbe5b6bba90ce0fd8f881cecc5960ee
Jesse Paroz <jesse.paroz@gmail.com>
2021-08-04 17:58:25 +1000

    Create directories on file save, if needed

--- lua/autocommands.lua
+++ lua/autocommands.lua
@@ -3,3 +3,10 @@ vim.cmd [[
 au TermOpen * setlocal nonumber
 au TermOpen * setlocal signcolumn=no
 ]]
+
+vim.cmd [[
+augroup Mkdir
+  autocmd!
+  autocmd BufWritePre * call mkdir(expand("<afile>:p:h"), "p")
+augroup END
+]]
