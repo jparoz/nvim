1a4ae2b5f9ef3344c08e644f77e1db7ac39af4fd
Jesse Paroz <jesse.paroz@gmail.com>
2021-10-26 21:05:00 +1000

    Jump to last known position on opening a file

--- lua/autocommands.lua
+++ lua/autocommands.lua
@@ -10,3 +10,11 @@ augroup Mkdir
   autocmd BufWritePre * call mkdir(expand("<afile>:p:h"), "p")
 augroup END
 ]]
+
+-- From the vim help files: *last-position-jump*
+vim.cmd [[
+augroup LastPositionJump
+  autocmd!
+  autocmd BufReadPost * if line("'\"") > 1 && line("'\"") <= line("$") && &ft !~# 'commit' | exe "normal! g`\"" | endif
+augroup END
+]]
