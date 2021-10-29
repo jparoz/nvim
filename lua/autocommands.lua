855ce39bd5623afc36bce65d0e489e493e315910
Jesse Paroz <jesse.paroz@gmail.com>
2021-10-29 13:15:13 +1000

    Document Mkdir autocommands

--- lua/autocommands.lua
+++ lua/autocommands.lua
@@ -4,12 +4,17 @@ au TermOpen * setlocal nonumber
 au TermOpen * setlocal signcolumn=no
 ]]
 
+
+-- Automatically create necessary directories when editing a new file
+-- e.g. creates directories foo and bar if they don't exist when running:
+-- :e foo/bar/baz.txt
 vim.cmd [[
 augroup Mkdir
   autocmd!
   autocmd BufWritePre * call mkdir(expand("<afile>:p:h"), "p")
 augroup END
 ]]
+
 
 -- From the vim help files: *last-position-jump*
 vim.cmd [[
