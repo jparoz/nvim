d0f973a4dc621e6a2a813da09b149d5263e966e1
Jesse Paroz <jesse.paroz@gmail.com>
2022-06-13 23:05:15 +1000

    Use Neovim 0.7's vim.keymap.set

--- ftplugin/todo.lua
+++ ftplugin/todo.lua
@@ -1,13 +1,11 @@
-local keymap = require("utils").keymap
-
 vim.bo.textwidth = 999
 vim.wo.linebreak = true
 vim.go.showbreak = "  "
 vim.wo.breakindent = true
 vim.wo.breakindentopt = "shift:2,sbr"
 
-keymap("n", "j", "gj", {buffer = true})
-keymap("n", "k", "gk", {buffer = true})
+vim.keymap.set("n", "j", "gj", {buffer = true})
+vim.keymap.set("n", "k", "gk", {buffer = true})
 
 function CheckUncheck()
     local matched = vim.fn.getline("."):match("^%s*([-x]) ")
@@ -20,4 +18,4 @@ function CheckUncheck()
     vim.cmd [[nohlsearch]]
 end
 
-keymap("n", "<CR>", "<CMD>lua CheckUncheck()<CR>", {buffer = true, silent = true})
+vim.keymap.set("n", "<CR>", "<CMD>lua CheckUncheck()<CR>", {buffer = true, silent = true})
