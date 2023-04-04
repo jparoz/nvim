8665674737a791dfa7fce4360a866f950929e2f3
Jesse Adamson Paroz <jesse.paroz@gmail.com>
2023-04-05 01:04:48 +1000

    Use concealcursor = "nc" in markdown files

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -2,6 +2,7 @@ vim.bo.tabstop = 2
 vim.bo.shiftwidth = 2
 vim.wo.linebreak = true
 vim.wo.conceallevel = 2
+vim.wo.concealcursor = "nc"
 vim.bo.textwidth = 0
 vim.wo.signcolumn = "yes"
 vim.o.showtabline = 2  -- always
