258ee4d40b3d425ecb9603606d021891c311ef48
Jesse Adamson Paroz <jesse.paroz@gmail.com>
2023-04-16 17:21:40 +1000

    Update LSPs for Lua and JSON

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -12,6 +12,33 @@ local on_attach = function(client, bufnr)
     vim.bo.omnifunc = "v:lua.vim.lsp.omnifunc"
 end
 
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+    vim.lsp.diagnostic.on_publish_diagnostics,
+    { severity_sort = true })
+
+vim.fn.sign_define("DiagnosticSignError",
+    { text = "●", texthl = "DiagnosticSignError" })
+vim.fn.sign_define("DiagnosticSignWarn",
+    { text = "○", texthl = "DiagnosticSignWarn" })
+vim.fn.sign_define("DiagnosticSignInfo",
+    { text = "ℹ", texthl = "DiagnosticSignInfo" })
+vim.fn.sign_define("DiagnosticSignHint",
+    { text = "➤", texthl = "DiagnosticSignHint" })
+
+-- Show a border around the hover window
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
+    vim.lsp.handlers.hover, {
+        border = "single",
+    }
+)
+
+-- Only show the first line of diagnostics as virtual text
+vim.diagnostic.config({virtual_text = {format = function(diag)
+    return diag.message:match("[^\n]+")
+end}})
+
+
+-- Server configuration below
 
 -- rust-analyzer
 lspconfig.rust_analyzer.setup {
@@ -71,73 +98,14 @@ lspconfig.marksman.setup {
     flags = flags,
 }
 
--- @Note: Sumneko Lua has been performing really poorly, mostly not working,
---        and just making my laptop whir. For now, just disabling.
-if false then -- sumneko Lua LSP setup
-    local sumneko_root_path = vim.env.HOME .. "/dev/projects/lua-language-server"
-    local sumneko_binary = sumneko_root_path .. "/bin/macOS/lua-language-server"
+-- lua-language-server
+lspconfig.lua_ls.setup {
+    on_attach = on_attach,
+    flags = flags,
+}
 
-    require("lspconfig").sumneko_lua.setup {
-        cmd = {sumneko_binary, "-E", sumneko_root_path .. "/main.lua"},
-        settings = {
-            Lua = {
-                runtime = {
-                    version = "LuaJIT", -- depends which file we're editing
-                    path = vim.split(package.path, ";"),
-                },
-                diagnostics = {
-                    globals = {"vim", "love", "hs"},
-                },
-                workspace = {
-                    library = {
-                        [vim.fn.expand('$VIMRUNTIME/lua')] = true,
-                        [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
-                    },
-                },
-            },
-        },
-        on_attach = function(client, bufnr)
-            if LSP_mappings then LSP_mappings(client, bufnr) end
-            vim.bo.omnifunc = "v:lua.vim.lsp.omnifunc"
-        end,
-
-        on_init = function(client)
-            local pwd = vim.fn.getcwd()
-            local lua_version = "LuaJIT"
-            if pwd:find(".hammerspoon") then
-                lua_version = "Lua 5.4"
-            end
-
-            client.config.settings.Lua.runtime.version = lua_version
-            client.notify("workspace/didChangeConfiguration")
-            return true
-        end,
-
-        flags = flags,
-    }
-end
-
-vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
-    vim.lsp.diagnostic.on_publish_diagnostics,
-    { severity_sort = true })
-
-vim.fn.sign_define("DiagnosticSignError",
-    { text = "●", texthl = "DiagnosticSignError" })
-vim.fn.sign_define("DiagnosticSignWarn",
-    { text = "○", texthl = "DiagnosticSignWarn" })
-vim.fn.sign_define("DiagnosticSignInfo",
-    { text = "ℹ", texthl = "DiagnosticSignInfo" })
-vim.fn.sign_define("DiagnosticSignHint",
-    { text = "➤", texthl = "DiagnosticSignHint" })
-
--- Show a border around the hover window
-vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
-    vim.lsp.handlers.hover, {
-        border = "single",
-    }
-)
-
--- Only show the first line of diagnostics as virtual text
-vim.diagnostic.config({virtual_text = {format = function(diag)
-    return diag.message:match("[^\n]+")
-end}})
+-- vscode-json-languageserver
+lspconfig.jsonls.setup {
+    on_attach = on_attach,
+    flags = flags,
+}
