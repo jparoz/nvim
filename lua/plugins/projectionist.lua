65eeacbcc15cb3163587e5ba3a79653376aaf358
Jesse Adamson Paroz <jesse.paroz@gmail.com>
2024-02-17 22:57:48 +1000

    Modify projectionist heuristics for Score One templates

--- lua/plugins/projectionist.lua
+++ lua/plugins/projectionist.lua
@@ -4,25 +4,44 @@ init = function()
     vim.g.projectionist_heuristics = {
         ["Templates/*/index.php"] = {
             ["Templates/*/index.php"] = { type = "php", related = {
-                "Templates/{}/board.js",
+                "Templates/{}/update.js",
                 "Templates/{}/style.css",
                 "Templates/{}/info.txt",
+                "Templates/{}/info.json",
             } },
-            ["Templates/*/board.js"] = { type = "js", related = {
+            ["Templates/*/update.js"] = { type = "js", related = {
                 "Templates/{}/index.php",
                 "Templates/{}/style.css",
                 "Templates/{}/info.txt",
+                "Templates/{}/info.json",
             } },
             ["Templates/*/style.css"] = { type = "css", related = {
                 "Templates/{}/index.php",
-                "Templates/{}/board.js",
+                "Templates/{}/update.js",
                 "Templates/{}/info.txt",
+                "Templates/{}/info.json",
             } },
             ["Templates/*/info.txt"] = { type = "txt", related = {
                 "Templates/{}/index.php",
-                "Templates/{}/board.js",
+                "Templates/{}/update.js",
                 "Templates/{}/style.css",
+                "Templates/{}/info.json",
             } },
+            ["Templates/*/info.json"] = { type = "json", related = {
+                "Templates/{}/index.php",
+                "Templates/{}/update.js",
+                "Templates/{}/style.css",
+                "Templates/{}/info.txt",
+            } },
+
+            ["schemas/*/v1.schema.json"] = {
+                type = "schema",
+                alternate = "schemas/{}/instance.json",
+            },
+            ["schemas/*/instance.json"] = {
+                type = "instance",
+                alternate = "schemas/{}/v1.schema.json",
+            },
         }
     }
 end,
