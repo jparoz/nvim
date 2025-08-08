db0f87a5ed56b3a4037302dcf40131302c8b31f7
Jesse Adamson Paroz <jesse@3ddataguidance.com>
2025-08-08 12:48:44 +1000

    Add a hotkey to toggle fullscreen in neovide

--- lua/neovide.lua
+++ lua/neovide.lua
@@ -10,3 +10,10 @@ vim.g.neovide_padding_right  = 10
 vim.g.neovide_padding_left   = 10
 
 vim.cmd [[cd ~]]
+
+-- Fullscreen toggle
+function ToggleFullscreen()
+    vim.g.neovide_fullscreen = not vim.g.neovide_fullscreen
+end
+
+vim.keymap.set("n", "<F11>", ToggleFullscreen)
