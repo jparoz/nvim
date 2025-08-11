a1af6d8a18d5180b79e07a493229a7c6e97b1d56
Jesse Adamson Paroz <jesse@3ddataguidance.com>
2025-08-11 11:29:01 +1000

    Remove duplicated todo-comments config

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -29,47 +29,6 @@ return {
         },
     },
 
-    { "folke/todo-comments.nvim",
-        dependencies = { "nvim-lua/plenary.nvim" },
-        opts = {
-            signs = false,
-            keywords = {
-                TODO = { icon = "●", color = "hint" },
-                NOTE = { icon = "ℹ", color = "hint", alt = { "INFO" } },
-                PERF = { icon = "●", color = "hint", alt = { "OPTIMIZE" } },
-                FIX = {
-                    icon = "●",
-                    color = "warning",
-                    alt = { "FIXME", "BUG", "FIXIT", "ISSUE", "CLEANUP" },
-                },
-                WARN = { icon = "●", color = "warning", alt = { "WARNING" } },
-                HACK = { icon = "●", color = "warning", alt = { "XXX" } },
-                TEST = {
-                    icon = "ℹ",
-                    color = "info",
-                    alt = { "TESTING", "PASSED", "FAILED" }
-                },
-            },
-            highlight = {
-                pattern = [[.*<(KEYWORDS)\s*:?]],
-                keyword = "bg",
-                after = "",
-            },
-            search = {
-                pattern = [[\b(KEYWORDS)\b]],
-            },
-        },
-        init = function(_plugin)
-            vim.keymap.set("n", "]t", function()
-                require("todo-comments").jump_next()
-            end, { desc = "Next todo comment" })
-
-            vim.keymap.set("n", "[t", function()
-                require("todo-comments").jump_prev()
-            end, { desc = "Previous todo comment" })
-        end,
-    },
-
     -- Medium (could live without these, but nice to have sometimes)
     { "godlygeek/tabular", cmd = "Tabularize" },
 
